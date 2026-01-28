21f5e663291f01667f1dab630436267ca52e7e21
Mathias Remshardt <remshams@gmail.com>
2026-01-28 09:58:37 +0100

    refactor: updates the description

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // Router configuration if needed
-  ]
+  ],
 };
